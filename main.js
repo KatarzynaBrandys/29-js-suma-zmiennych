fd6b52f91088cad80fd48b6c20850d157fa91dbb
KatarzynaBrandys <68773576+KatarzynaBrandys@users.noreply.github.com>
2020-12-16 17:17:53 +0100

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstNumber = 50;
+let secondNumber = 120;
+
+function sum(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+console.log(sum(firstNumber,secondNumber));
+
